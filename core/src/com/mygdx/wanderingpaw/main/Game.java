2e00ab7d72bd72b8ec38f1c33847fdd85c08c39f
Shahriar Alvi <alam.shahriaralvi@gmail.com>
2022-05-28 14:02:30 +0600

    Setting up

--- core/src/com/mygdx/wanderingpaw/main/Game.java
+++ core/src/com/mygdx/wanderingpaw/main/Game.java
@@ -2,49 +2,85 @@ package com.mygdx.wanderingpaw.main;
 
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.wanderingpaw.handlers.GameStateManager;
 
 
 public class Game implements ApplicationListener {
 
-	public static final String TITLE = "Block Bunny";
-	public static final int V_WIDTH = 320;
-	public static final int V_HEIGHT = 240;
-	public static final int SCALE = 2;
-	public static final float STEP = 1 / 60f;
-
-	private SpriteBatch sb;
-	private OrthographicCamera cam;
-	private OrthographicCamera hudCam;
+    public static final String TITLE = "Block Bunny";
+    public static final int V_WIDTH = 320;
+    public static final int V_HEIGHT = 240;
+    public static final int SCALE = 2;
+    public static final float STEP = 1 / 60f;
+    private float accum;
 
 
 
-	@Override
-	public void create () {
-	}
+    private SpriteBatch sb;
+    private OrthographicCamera cam;
+    private OrthographicCamera hudCam;
 
-	@Override
-	public void resize(int width, int height) {
+    public SpriteBatch getSpriteBatch() {
+        return sb;
+    }
 
-	}
+    public OrthographicCamera getCamera() {
+        return cam;
+    }
 
-	@Override
-	public void render () {
+    public OrthographicCamera getHUDCamera() {
+        return hudCam;
+    }
 
-	}
 
-	@Override
-	public void pause() {
+    @Override
+    public void create() {
 
-	}
+        sb = new SpriteBatch();
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, V_WIDTH, V_HEIGHT);
 
-	@Override
-	public void resume() {
+        hudCam = new OrthographicCamera();
+        hudCam.setToOrtho(false, V_WIDTH, V_HEIGHT);
 
-	}
 
-	@Override
-	public void dispose () {
-	}
+        gsm = new GameStateManager(this);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    private GameStateManager gsm;
+
+    @Override
+    public void render() {
+
+        accum += Gdx.graphics.getDeltaTime();
+
+        while(accum >= STEP){
+            accum -= STEP;
+            gsm.update(STEP);
+            gsm.render();
+        }
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
---
+++ core/src/com/mygdx/wanderingpaw/states/GameState.java
@@ -0,0 +1,32 @@
+package com.mygdx.wanderingpaw.states;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.wanderingpaw.handlers.GameStateManager;
+import com.mygdx.wanderingpaw.main.Game;
+
+public abstract class GameState {
+
+    protected GameStateManager gsm;
+    protected Game game;
+
+    protected SpriteBatch sb;
+    protected OrthographicCamera cam;
+    protected OrthographicCamera hudCam;
+
+    protected GameState(GameStateManager gsm){
+        this.gsm = gsm;
+        game = gsm.game();
+        sb = game.getSpriteBatch();
+        cam = game.getCamera();
+        hudCam = game.getHUDCamera();
+
+    }
+
+    public abstract void handleInput();
+    public abstract void update(float dt);
+    public abstract void render();
+    public abstract void dispose();
+
+
+}
